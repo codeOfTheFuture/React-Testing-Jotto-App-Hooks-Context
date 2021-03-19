7380e1fe01b7edc81ff799f8dc9a3333975b382f
Jeff Oliver <jeffoliver3384@gmail.com>
2021-03-18 22:28:21 -0400

    Wrote tests for useEffect and useReducer hook and code to pass tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
 
+import hookActions from "./actions/hookActions";
+
+const mockGetSecretWord = jest.fn();
+
 /**
  * Setup function for app component.
- * @returns {ShallowWrapper}
+ * @returns {ReactWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  return mount(<App />);
 };
 
 test("App renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-app");
   expect(component.length).toBe(1);
+});
+
+describe("getSecretWord calls", () => {
+  test("getSecretWord gets called on App mount", () => {
+    setup();
+    // don't need to save the output of setup
+    // don't care about the wrapper itself only that getSecretWord mock was run
+    // check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
